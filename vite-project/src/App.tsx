5b911d73905fc2dfef521167c7365ff8290b1eb5
Dustin Santoso <dustin.santoso025@gmail.com>
2025-05-23 20:05:28 -0700

    Update App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -19,6 +19,15 @@ function App() {
         </h2>
       </div>
       <Cards></Cards>
+      <div className="py-10"></div>
+      <div className="py-2">
+        <h2 className="jusimt-10 scroll-m-20 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-left py-5 px-5 ">
+          Team Projects
+        </h2>
+      </div>
+      <Cards></Cards>
+      <div className="py-10"></div>
+      <div className="py-10 padding-10 w-full border-t border-gray-200 dark:border-gray-700 bg-background"></div>
     </div>
   );
 }
