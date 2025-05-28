442dd3b6057da437db61b3836b2d04ee65b8e855
Dustin Santoso <dustin.santoso025@gmail.com>
2025-05-27 20:08:36 -0700

    about me page

--- vite-project/src/pages/AboutMe.tsx
+++ vite-project/src/pages/AboutMe.tsx
@@ -1,30 +1,42 @@
+import { CarouselSize } from "@/components/Carousel";
 import winter from "../assets/winter.jpg";
-
+import { Carousel } from "@/components/ui/carousel";
 function AboutMe() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center pt-8 px-8 md:px-30 md:gap-x-12">
-      {/* Text Section */}
-      <div className="md:w-1/2 text-center md:text-left">
-        <h2 className="text-4xl font-bold">About Me</h2>
-        <p className="text-lg text-gray-700">
-          Hi, I'm Dustin a recent Computer Science graduate with a passion for
-          data engineering, systems design, and creating intuitive user
-          experiences.
-        </p>
-        <p className="text-lg text-gray-700">
-          I'm currently open to new opportunities in software or data
-          engineering. Outside of work, I enjoy building games, exploring new
-          tech, and learning from challenges.
-        </p>
+    <div className="flex flex-col items-center md:px-30">
+      {/* Main Content Section */}
+      <div className="flex flex-col md:flex-row items-center justify-center pt-8 md:gap-x-12">
+        {/* Text Section */}
+        <div className="md:w-1/2 text-center md:text-left">
+          <h2 className="text-4xl font-bold">About Me</h2>
+          <p className="text-lg text-gray-700">
+            Hi, I'm Dustin a recent Computer Science graduate with a passion for
+            data engineering, systems design, and creating intuitive user
+            experiences. My career trajectory has always been evolving with each
+            new experience. Initially a med-school prospect turned to a computer
+            science student turned to an aspiring data engineer. I am excited to
+            see where this path may lead me.
+          </p>
+          <p className="text-lg text-gray-700 mt-4">
+            Outside of work, I love obsessing over hobbies whether it be
+            competitive video games, skincare, or designing my room. If it is
+            something that peaks my interest, then I am diving head first.
+          </p>
+        </div>
+
+        {/* Image Section */}
+        <div className="h-[75vh] overflow-hidden rounded-2xl shadow-lg">
+          <img
+            src={winter}
+            alt="Dustin Santoso"
+            className="h-full w-full object-cover"
+          />
+        </div>
       </div>
 
-      {/* Image Section */}
-      <div className="h-[75vh] overflow-hidden rounded-2xl shadow-lg">
-        <img
-          src={winter}
-          alt="Dustin Santoso"
-          className="h-full w-full object-cover"
-        />
+      <div className="py-10 "></div>
+      <div className="w-full px-30 py-10 border-t h-[50vh]">
+        <CarouselSize></CarouselSize>
       </div>
     </div>
   );
